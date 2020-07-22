847c78d1e98d74a8fab273331bf9263c047e8de2
vinisimoes <vini.lopes.simoes@gmail.com>
2020-07-22 13:44:58 -0300

    Visual finalizado

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+    function getPClassName() {
+        if (props.item.completed) {
+            return "completed";
+        }
+        return "";
+    }
+
     return (
         <div className="todo-item">
             <input type="checkbox" checked={props.item.completed} onChange={() => props.handleChange(props.item.id)} />
-            <p>{props.item.task}</p>
+            <p className={getPClassName()}>{props.item.task}</p>
         </div>
     );
 }
