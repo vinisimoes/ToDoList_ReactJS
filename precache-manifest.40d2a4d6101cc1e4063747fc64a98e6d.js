840d8721faf1388b5bac6346e5437aa025a0a6e6
vinisimoes <vini.lopes.simoes@gmail.com>
2020-07-23 11:30:26 -0300

    Updates

---
+++ precache-manifest.40d2a4d6101cc1e4063747fc64a98e6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca07e0869fa32b03aab1ac2eca68d2b",
+    "url": "/ToDoList_ReactJS/index.html"
+  },
+  {
+    "revision": "333f6f88f537dd028e58",
+    "url": "/ToDoList_ReactJS/static/css/main.c4c7d701.chunk.css"
+  },
+  {
+    "revision": "8a024fbf94dfc480f18b",
+    "url": "/ToDoList_ReactJS/static/js/2.f9937aaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList_ReactJS/static/js/2.f9937aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333f6f88f537dd028e58",
+    "url": "/ToDoList_ReactJS/static/js/main.a4de8bbb.chunk.js"
+  },
+  {
+    "revision": "a695a34e020c580be4bf",
+    "url": "/ToDoList_ReactJS/static/js/runtime-main.972ba848.js"
+  },
+  {
+    "revision": "437da0b057d65d585a09af08abbb8397",
+    "url": "/ToDoList_ReactJS/static/media/add.437da0b0.svg"
+  },
+  {
+    "revision": "8dbcedd7f8060ac53bcd06d9b2d75293",
+    "url": "/ToDoList_ReactJS/static/media/trash.8dbcedd7.svg"
+  }
+]);
